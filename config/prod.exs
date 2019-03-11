624cf8fbbc546a84d645b1362bee6a7bdaf5eca7
João Fernandes <joaovitorfernandes2@gmail.com>
2019-03-11 11:48:25 -0300

    Add prod port and url configs

--- config/prod.exs
+++ config/prod.exs
@@ -71,6 +71,9 @@ config :logger, level: :info
 # import_config "prod.secret.exs"
 
 config :youtube_stats, YoutubeStatsWeb.Endpoint,
+       http: [port: System.get_env("PORT")],
+       url: [scheme: "https", host: "murmuring-beach-89239.herokuapp.com", port: 443],
+       force_ssl: [rewrite_on: [:x_forwarded_proto]],
        cache_static_manifest: "priv/static/cache_manifest.json",
        secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
