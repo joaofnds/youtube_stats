345182f30c9e7554939d262caf4f668c181cb037
João Fernandes <joaovitorfernandes2@gmail.com>
2019-03-11 11:09:25 -0300

    Change prod config to deploy on heroku

--- config/prod.exs
+++ config/prod.exs
@@ -68,4 +68,13 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which should be versioned
 # separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
+
+config :youtube_stats, YoutubeStatsWeb.Endpoint,
+       cache_static_manifest: "priv/static/cache_manifest.json",
+       secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+
+config :youtube_stats, YoutubeStats.Repo,
+       url: System.get_env("DATABASE_URL"),
+       pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+       ssl: true
