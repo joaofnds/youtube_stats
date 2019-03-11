5605ab66e7f83f8793ec2f8f9f8d640ebacb866a
João Fernandes <joaovitorfernandes2@gmail.com>
2019-03-11 08:43:23 -0300

    Publish last war stats to the socket when a user joins

--- lib/youtube_stats_web/channels/stats_channel.ex
+++ lib/youtube_stats_web/channels/stats_channel.ex
@@ -2,6 +2,12 @@ defmodule YoutubeStatsWeb.StatsChannel do
   use YoutubeStatsWeb, :channel
 
   def join("stats", _payload, socket) do
+    send(self(), :after_join)
     {:ok, socket}
+  end
+
+  def handle_info(:after_join, socket) do
+    push(socket, "war", YoutubeStats.War.last_stats())
+    {:noreply, socket}
   end
 end
