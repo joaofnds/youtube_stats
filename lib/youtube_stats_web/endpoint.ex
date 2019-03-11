27f7d13e21a9250471c4b4aff11b4a52eb4a51bf
João Fernandes <joaovitorfernandes2@gmail.com>
2019-03-11 11:49:47 -0300

    decrease the timeout for the websocket transport

--- lib/youtube_stats_web/endpoint.ex
+++ lib/youtube_stats_web/endpoint.ex
@@ -2,7 +2,7 @@ defmodule YoutubeStatsWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :youtube_stats
 
   socket "/socket", YoutubeStatsWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
